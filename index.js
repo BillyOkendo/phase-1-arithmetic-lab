5d9218338bd05e3780f384c9195b746f3a6f538d
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-06-08 00:03:23 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+const num3 = 28
+const num4 = 6
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() / 7) + 13
+
+const mod = num3%num4
+
+const max = Math.max(4, 17, 12, 8, 20, 19)
